86807c290ba80810670dfac87edba2ed02523541
KimJeongGook <gooki1210@naver.com>
2022-04-11 15:59:22 +0900

    lombok 설정 @Getter, @Setter

--- Potal/src/main/java/co/homework/potal/board/service/BoardVO.java
+++ Potal/src/main/java/co/homework/potal/board/service/BoardVO.java
@@ -2,6 +2,11 @@ package co.homework.potal.board.service;
 
 import java.sql.Date;
 
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
 public class BoardVO {
 
 	private int no;
@@ -12,65 +17,5 @@ public class BoardVO {
 	private int hit;
 	
 	private String name;
-	
-	public BoardVO() {
-
-	}
-
-	public int getNo() {
-		return no;
-	}
-
-	public void setNo(int no) {
-		this.no = no;
-	}
-
-	public String getWriter() {
-		return writer;
-	}
-
-	public void setWriter(String writer) {
-		this.writer = writer;
-	}
-
-	public Date getWdate() {
-		return wdate;
-	}
-
-	public void setWdate(Date wdate) {
-		this.wdate = wdate;
-	}
-
-	public String getTitle() {
-		return title;
-	}
-
-	public void setTitle(String title) {
-		this.title = title;
-	}
-
-	public String getSubject() {
-		return subject;
-	}
-
-	public void setSubject(String subject) {
-		this.subject = subject;
-	}
-
-	public int getHit() {
-		return hit;
-	}
-
-	public void setHit(int hit) {
-		this.hit = hit;
-	}
-
-	public String getName() {
-		return name;
-	}
-
-	public void setName(String name) {
-		this.name = name;
-	}
 	
 }
--- Potal/src/main/java/co/homework/potal/member/service/MemberVO.java
+++ Potal/src/main/java/co/homework/potal/member/service/MemberVO.java
@@ -1,5 +1,10 @@
 package co.homework.potal.member.service;
 
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
 public class MemberVO {
 
 	private String id;
@@ -8,47 +13,5 @@ public class MemberVO {
 	private String phone;
 	private String address;
 	private String author;
-	
-	public MemberVO() {
-		
-	}
-	
-	public String getId() {
-		return id;
-	}
-	public void setId(String id) {
-		this.id = id;
-	}
-	public String getPassword() {
-		return password;
-	}
-	public void setPassword(String password) {
-		this.password = password;
-	}
-	public String getName() {
-		return name;
-	}
-	public void setName(String name) {
-		this.name = name;
-	}
-	public String getPhone() {
-		return phone;
-	}
-	public void setPhone(String phone) {
-		this.phone = phone;
-	}
-	public String getAddress() {
-		return address;
-	}
-	public void setAddress(String address) {
-		this.address = address;
-	}
-	public String getAuthor() {
-		return author;
-	}
-	public void setAuthor(String author) {
-		this.author = author;
-	}
-	
 	
 }
